2a7aa63cc018d0e9e061a9c8e6cd6398109e1b3f
Amber Williams <awilliams1234@live.com>
2020-03-11 18:53:58 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,10 +14,26 @@ class Transfer
   end 
   
   def execute_transaction
+    if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+  
+    elsif @status == "complete"
+      puts "Transaction was already executed"
+  
+    else
+      @sender.balance > @amount && @status = "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    end 
   end 
   
   def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end 
   end 
-  
-    
 end
