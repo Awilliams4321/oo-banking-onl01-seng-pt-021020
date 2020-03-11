2c91c63b918bf2413f8696de6834f594795284c4
Amber Williams <awilliams1234@live.com>
2020-03-11 17:59:45 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,11 +9,19 @@ class BankAccount
     @status = "open"
   end 
   
-  def deposit 
+  def deposit(money) 
+    @balance += money
+  end 
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end 
+  
+  def valid?
   end 
   
   def close_account
-    self.all.clear
+    self.all.cancel 
   end 
 
 end
